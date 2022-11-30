d7e40e92618e2df95f4dba86ada32891fa747263
Dymond <dymond.hoffman03@gmail.com>
2022-11-30 13:35:21 -0800

    building App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import Header from "./components/Header";
-import Project from "./components/Project";
-import Navigation from "./components/Navigation";
-import Footer from "./components/Footer";
+
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import About from './components/About';
+import Project from './components/Project';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+
+
 
 function App() {
+  const [currentTab, setCurrentTab] = useState("about")
+
+
+  
   return (
     <div>
       < Header />
       < Navigation />
+      < About />
       < Project />
+      < Contact />
+      < Resume />
       < Footer />
 
     </div>
