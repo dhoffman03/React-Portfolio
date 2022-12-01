4a179d8f4785aeb2075879dfb06144855e654f78
Dymond <dymond.hoffman03@gmail.com>
2022-11-30 21:50:56 -0800

    Footer outlined, css still needed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,21 @@
-function Footer() {}
+import React from "react";
+import { FiLinkedin } from "react-icons/fi";
+import { FiGithub } from "react-icons/fi";
+import { DiStackoverflow } from "react-icons/di";
+
+
+function Footer() {
+    return (
+        <footer>
+            <h2>Designed by Dymond Hoffman</h2>
+            <ul>
+                <li><a href="https://github.com/dhoffman03">< FiGithub /></a></li>
+                <li><a href="https://www.linkedin.com/in/dymondhoffman/">< FiLinkedin /></a></li>
+                <li><a href="https://stackoverflow.com/users/19372576/dymond-h">< DiStackoverflow /></a></li>
+            </ul>
+
+        </footer>
+    )
+}
 
 export default Footer;
-
-// Requirements
-    // icons to LinkedIn, GitHub, and Stack Overflow
