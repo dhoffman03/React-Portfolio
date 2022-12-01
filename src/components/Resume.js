ecff3d5b8071a32d2cff8a57461aa388bb107404
Dymond <dymond.hoffman03@gmail.com>
2022-12-01 02:30:16 -0800

    resume built, css needed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React from 'react';
+import myResume from '../assets/files/Dymond_Hoffman_Resume.pdf'
+import { FiDownloadCloud } from "react-icons/fi";
 
-function Resume() {}
+function Resume() {
+    return (
+        <div>
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">Developer Proficiencies</h2>
+                    <div>
+                        <p>Front-end Proficiencies</p>
+                        <ul>
+                            <li>HTML5</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>React</li>
+                            <li>APIs</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <p>Back-end Proficiencies</p>
+                        <ul>
+                            <li>MySQL</li>
+                            <li>Node.js</li>
+                            <li>Express.js</li>
+                            <li>MongoDB</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h2 class="card-title">Resume</h2>
+                    <div>
+                        <a href={myResume} download><FiDownloadCloud /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Resume;
-
-// Requirements
-    // link to download resume
-    // list of proficiencies
