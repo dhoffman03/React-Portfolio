68850aff718e7b3709024ae7726b3966e9ac2045
Dymond <dymond.hoffman03@gmail.com>
2022-12-01 14:02:05 -0800

    about page css

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,17 @@ import avatar from '../assets/images/avatar.png'
 function About() {
     return (
         <section>
-            <h2>About Me</h2>
-            <div>
-                <div>
+            <h2 className='about text-center'>About Me</h2>
+            <div className='d-flex align-items-center justify-content-evenly'>
+                <div className='about-profile text-center'>
                     <img src={avatar} alt='Dymond Hoffman' />
                 </div>
-                <p>
-                    Hey there! My name is Dymond. I am currently a student of UofM Coding Bootcamp. I am still new to coding and have just started my web development journey. I am eager to expand my coding skills and become a professional web developer. When I'm not coding, you can find me painting, working with clay, or napping with my cats.
-                </p>
+                <div className='bio'>
+                    <p>
+                        Hey there! My name is Dymond. I am currently a student of UofM Coding Bootcamp. I am still new to coding and have just started my web development journey. I am eager to expand my coding skills and become a professional web developer. When I'm not coding, you can find me painting, working with clay, or napping with my cats.
+                    </p>
+                </div>
+
             </div>
         </section>
     )
