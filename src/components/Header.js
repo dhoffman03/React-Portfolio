9ae34c7007ebf3df7caf8d84df265a3da2498eba
Dymond <dymond.hoffman03@gmail.com>
2022-11-30 22:16:54 -0800

    header built, css still needed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,18 @@ function Header(props) {
     const { currentTab, setCurrentTab } = props;
 
     return (
-        
+        <header>
+            <div>
+                <h1>Dymond Hoffman</h1>
+            </div>
+            <div>
+                <Navigation
+                    currentTab={currentTab}
+                    setCurrentTab={setCurrentTab}
+                ></Navigation>
+            </div>
+        </header>
     )
 }
 
 export default Header;
-
-// Requirements
-    // Dev name
-    // Nav titles 
