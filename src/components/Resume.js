a6323e105639c99f589271d7a0c4f23da893722a
Dymond <dymond.hoffman03@gmail.com>
2022-12-01 15:38:44 -0800

    resume css

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,40 +4,40 @@ import { FiDownloadCloud } from "react-icons/fi";
 
 function Resume() {
     return (
-        <div>
+        <div className='text-center'>
             <div class="card">
                 <div class="card-body">
-                    <h2 class="card-title">Developer Proficiencies</h2>
-                    <div>
-                        <p>Front-end Proficiencies</p>
-                        <ul>
-                            <li>HTML5</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                            <li>React</li>
-                            <li>APIs</li>
-                        </ul>
+                    <h2 class="card-title title text-center">Developer Proficiencies</h2>
+                    <div className=''>
+                        <div className='box'>
+                            <p className='subtitle'>Front-end Proficiencies</p>
+                            <ul>
+                                <li className='list'>HTML5</li>
+                                <li className='list'>CSS</li>
+                                <li className='list'>JavaScript</li>
+                                <li className='list'>React</li>
+                                <li className='list'>APIs</li>
+                            </ul>
+                        </div>
+
+                        <div className='box'>
+                            <p className='subtitle'>Back-end Proficiencies</p>
+                            <ul>
+                                <li className='list'>MySQL</li>
+                                <li className='list'>Node.js</li>
+                                <li className='list'>Express.js</li>
+                                <li className='list'>MongoDB</li>
+                            </ul>
+                        </div>
                     </div>
 
-                    <div>
-                        <p>Back-end Proficiencies</p>
-                        <ul>
-                            <li>MySQL</li>
-                            <li>Node.js</li>
-                            <li>Express.js</li>
-                            <li>MongoDB</li>
-                        </ul>
-                    </div>
 
                 </div>
             </div>
 
             <div class="card mb-3">
-                <div class="card-body">
-                    <h2 class="card-title">Resume</h2>
-                    <div>
-                        <a href={myResume} download><FiDownloadCloud /></a>
-                    </div>
+                <div class="card-body download">
+                        <a href={myResume} download>Download My Resume!<FiDownloadCloud /></a>
                 </div>
             </div>
         </div>
