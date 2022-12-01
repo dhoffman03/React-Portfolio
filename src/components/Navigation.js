6db7b1f742a4ed46f188407e3c8d0f869532f0c0
Dymond <dymond.hoffman03@gmail.com>
2022-11-30 22:35:14 -0800

    building nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,33 @@
-function Navigation() {}
+import React from "react";
+
+function Navigation(props) {
+    const { currentTab, setCurrentTab } = props;
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-nav">
+                <a href="#about"
+                    onClick={() => setCurrentTab('About')}
+                    className={currentTab === 'About' ? 'nav-link-active' : 'nav-link'}
+                >About Me</a>
+
+                <a href="#projects"
+                >Portfolio</a>
+
+                <a href="#contact"
+                >Contact</a>
+
+                <a href="#resume"
+                >Resume</a>
+            </div>
+        </nav>
+    )
+}
 
 export default Navigation;
 
 // Requirements
-    // About Me -- Default section
+    // About Me -- Defadivt section
     // Portfolio
     // Contact
     // Resume
